5c5b55c436813e11d37889c6b19e6cd98269057e
josh111112 <josh.johnson0388@gmail.com>
2024-11-14 16:58:07 -0500

    pushing the main game file

---
+++ game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "item.hpp"    
+#include "location.hpp"
+#include "npc.hpp"     
+
+
+
+
+
+int main(){
+	bool gameRunning = true;
+	while (gameRunning){
+		// the is the main game file 
+
+
+	}
+
+
+	return 0;
+}
