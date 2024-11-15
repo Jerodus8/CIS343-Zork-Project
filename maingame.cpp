cc2bc6780e3e9497baf6dc1771c2c0b3a247a41e
josh111112 <josh.johnson0388@gmail.com>
2024-11-15 13:14:03 -0500

    fixed the syntax errors and bugs, also did convert to utf-8 bc g++ compiler cant use utf-16, so if theres some strange bugs that could be why shouldnt be an issue though

---
+++ maingame.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "item.hpp"
+#include "location.hpp"
+#include "npc.hpp"
+
+
+
+
+int main(){
+
+	std::cout << "Hello, world!" << std::endl;
+	bool gameRunning = true;
+	while (gameRunning){
+		// the is the main game file 
+		std::cout << "Hello, world!" << std::endl;
+		gameRunning = false;
+	}
+	return 0;
+}
